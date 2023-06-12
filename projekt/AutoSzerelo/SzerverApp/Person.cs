78975e0e1a7f54a9ab4ca86ee528f77ccc95e1e7
Izayata <moonwolf99public@gmail.com>
2023-06-12 16:27:50 +0200

    Validáció gyakorlása DataAnnotation-ök segítségével

--- projekt/AutoSzerelo/SzerverApp/Person.cs
+++ projekt/AutoSzerelo/SzerverApp/Person.cs
@@ -1,12 +1,20 @@
-﻿namespace SzerverApp
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SzerverApp
 {
     public class Person
     {
+        /// Validáció
         public int Id { get; set; }
+
+        [Required]
+        [MaxLength(25)]
         public string Name { get; set; }
 
+        [EmailAddress]
         public string Email { get; set; }
 
-        public DateTime DateOfBirth { get; set; }
+        [Required]
+        public DateTime? BirthDate { get; set; }
     }
 }
