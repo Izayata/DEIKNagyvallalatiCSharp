bf685d1b6de7933288dbbfd8d8562ccd9a0ceedf
Izayata <moonwolf99public@gmail.com>
2023-06-13 11:54:04 +0200

    Person.Id beállítása elsődleges kulcsként a DB-hez

--- projekt/AutoSzerelo/SzerverApp/Person.cs
+++ projekt/AutoSzerelo/SzerverApp/Person.cs
@@ -1,21 +1,24 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace SzerverApp
 {
     public class Person
     {
         /// Validáció
+        [Key] //Elsődleges kulcs beállítása
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)] //automatikus kulcs generálás
         public int Id { get; set; }
 
         [Required]
-        [MaxLength(25)]
+        //[MaxLength(25)]
         public string Name { get; set; }
 
         [EmailAddress]
         public string Email { get; set; }
 
         [Required]
-        [Range(typeof(DateTime), "1990-01-01", "2000-01-01")]
+        //[Range(typeof(DateTime), "1990-01-01", "2000-01-01")]
         public DateTime? BirthDate { get; set; }
     }
 }
